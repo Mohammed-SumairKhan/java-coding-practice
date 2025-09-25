5c2d5c0423d907b6d2ce978d033399d0e7714ac8
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-25 23:05:43 +0530

    Add UpperCase()

---
+++ src/com/programs/UpperCase.java
@@ -0,0 +1,8 @@
+package com.programs;
+
+public class UpperCase {
+	public static void upperCase(String str) {
+		String result = str.toUpperCase(); 
+		System.out.println("Uppercase String: " + result);
+	}
+}

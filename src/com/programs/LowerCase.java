bc2db34d475af32b6c909cadbe62fed9f5b36e00
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-25 23:05:24 +0530

    Add UpperCase()

---
+++ src/com/programs/LowerCase.java
@@ -0,0 +1,8 @@
+package com.programs;
+
+public class LowerCase {
+	public static void lowercase(String str) {
+		String result = str.toLowerCase(); 
+		System.out.println("Lowercase String: " + result);
+	}
+}
